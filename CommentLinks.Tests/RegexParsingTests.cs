c5a5452f43d2caffc7e5fc11fc4027e5092ba4eb
Matt Lacey <matt@mrlacey.co.uk>
2021-02-20 13:55:22 +0000

    add tests reflecting examples from VS Marketplace Q&A

--- CommentLinks.Tests/RegexParsingTests.cs
+++ CommentLinks.Tests/RegexParsingTests.cs
@@ -27,6 +27,51 @@ namespace CommentLinks.Tests
             Assert.AreEqual("SMART_LOG_DEVICE_STATISTICS;", sut.SearchTerm);
         }
 
+        [TestMethod]
+        public void CanHaveUnderscoreInSearchTerm_WithQuotesAroundSearchTerm()
+        {
+            var sut = ExtractTagFromLine("blah blah blah link:cDriveCommands.cpp:\"SMART_LOG_DEVICE_STATISTICS\"");
+
+            Assert.IsNotNull(sut);
+            Assert.AreEqual("cDriveCommands.cpp", sut.FileName);
+            Assert.AreEqual(-1, sut.LineNo);
+            Assert.AreEqual("\"SMART_LOG_DEVICE_STATISTICS\"", sut.SearchTerm);
+        }
+
+        [TestMethod]
+        public void CanHaveSemicolonInSearchTerm_WithQuotesAroundSearchTerm()
+        {
+            var sut = ExtractTagFromLine("blah blah blah link:cDriveCommands.cpp:\"SMART_LOG_DEVICE_STATISTICS;\"");
+
+            Assert.IsNotNull(sut);
+            Assert.AreEqual("cDriveCommands.cpp", sut.FileName);
+            Assert.AreEqual(-1, sut.LineNo);
+            Assert.AreEqual("\"SMART_LOG_DEVICE_STATISTICS;\"", sut.SearchTerm);
+        }
+
+
+        [TestMethod]
+        public void CanHaveUnderscoreInSearchTerm_WithQuotesAroundAll()
+        {
+            var sut = ExtractTagFromLine("blah blah blah link:\"cDriveCommands.cpp:SMART_LOG_DEVICE_STATISTICS\"");
+
+            Assert.IsNotNull(sut);
+            Assert.AreEqual("cDriveCommands.cpp", sut.FileName);
+            Assert.AreEqual(-1, sut.LineNo);
+            Assert.AreEqual("SMART_LOG_DEVICE_STATISTICS", sut.SearchTerm);
+        }
+
+        [TestMethod]
+        public void CanHaveSemicolonInSearchTerm_WithQuotesAroundAll()
+        {
+            var sut = ExtractTagFromLine("blah blah blah link:\"cDriveCommands.cpp:SMART_LOG_DEVICE_STATISTICS;\"");
+
+            Assert.IsNotNull(sut);
+            Assert.AreEqual("cDriveCommands.cpp", sut.FileName);
+            Assert.AreEqual(-1, sut.LineNo);
+            Assert.AreEqual("SMART_LOG_DEVICE_STATISTICS;", sut.SearchTerm);
+        }
+
         private CommentLinkTag ExtractTagFromLine(string line)
         {
             var regex = RegexHelper.LinkRegex;
@@ -36,6 +81,7 @@ namespace CommentLinks.Tests
             if (matches.Count > 0)
             {
                 // Mirroring behavior in link:CommentLinkTagger.cs:ExtractTagFromLine
+                // Not reusing the actual classes from teh extension becuase they have dependencies on VS that are hard to mock/abstract
                 return new CommentLinkTag(matches[0].Groups[2].Value);
             }
             else
