66437e477a825a47872822382c46d88907c8410c
Matt Lacey <matt@mrlacey.co.uk>
2021-03-02 16:08:48 +0000

    Try and ensure the package has loaded before accessing options

--- src/CommentLinkTaggerProvider.cs
+++ src/CommentLinkTaggerProvider.cs
@@ -3,6 +3,8 @@
 
 using System;
 using System.ComponentModel.Composition;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
 using Microsoft.VisualStudio.Text;
 using Microsoft.VisualStudio.Text.Editor;
 using Microsoft.VisualStudio.Text.Tagging;
@@ -19,9 +21,22 @@ namespace CommentLinks
         public ITagger<T> CreateTagger<T>(ITextBuffer buffer)
             where T : ITag
         {
+            ThreadHelper.ThrowIfNotOnUIThread();
             if (buffer == null)
             {
                 throw new ArgumentNullException("buffer");
+            }
+
+            if (CommentLinksPackage.Instance == null)
+            {
+                // Try and force load the project if it hasn't already loaded
+                // so can access the configured options.
+                if (ServiceProvider.GlobalProvider.GetService(typeof(SVsShell)) is IVsShell shell)
+                {
+                    // IVsPackage package = null;
+                    Guid PackageToBeLoadedGuid = new Guid(CommentLinksPackage.PackageGuidString);
+                    shell.LoadPackage(ref PackageToBeLoadedGuid, out _);
+                }
             }
 
             var keyword = CommentLinksPackage.Instance?.Options?.TriggerWord;
