009f0e77a83ae2015c719b9d0657fa18c35ece0a
Matt Lacey <matt@mrlacey.co.uk>
2022-03-01 21:39:53 +0000

    async improvements

--- src/SponsorRequestHelper.cs
+++ src/SponsorRequestHelper.cs
@@ -3,6 +3,7 @@
 
 using System;
 using System.Threading.Tasks;
+using Microsoft.VisualStudio.Shell;
 
 namespace CommentLinks
 {
@@ -10,6 +11,8 @@ namespace CommentLinks
     {
         public static async Task CheckIfNeedToShowAsync()
         {
+            await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync();
+
             if (await SponsorDetector.IsSponsorAsync())
             {
                 if (new Random().Next(1, 10) == 2)
@@ -25,6 +28,8 @@ namespace CommentLinks
 
         private static void ShowThanksForSponsorshipMessage()
         {
+            ThreadHelper.ThrowIfNotOnUIThread();
+
             GeneralOutputPane.Instance.WriteLine("Thank you for your sponsorship. It really helps.");
             GeneralOutputPane.Instance.WriteLine("If you have ideas for new features or suggestions for new features");
             GeneralOutputPane.Instance.WriteLine("please raise an issue at https://github.com/mrlacey/CommentLinks/issues");
@@ -33,6 +38,8 @@ namespace CommentLinks
 
         private static void ShowPromptForSponsorship()
         {
+            ThreadHelper.ThrowIfNotOnUIThread();
+
             GeneralOutputPane.Instance.WriteLine("Sorry to interrupt. I know your time is busy, presumably that's why you installed this extension (Comment Links).");
             GeneralOutputPane.Instance.WriteLine("I'm happy that the extensions I've created have been able to help you and many others");
             GeneralOutputPane.Instance.WriteLine("but I also need to make a living, and two years without work and extended periods of illness have been a challenge. - I didn't qualify for any government support either. :(");
