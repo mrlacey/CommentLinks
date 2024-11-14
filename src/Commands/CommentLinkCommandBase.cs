a88a2454cbb054793aefbb0e152da07f53344084
Matt Lacey <matt@mrlacey.co.uk>
2024-11-14 16:45:37 +0000

    Add `UseAbsolutePaths` option

--- src/Commands/CommentLinkCommandBase.cs
+++ src/Commands/CommentLinkCommandBase.cs
@@ -8,76 +8,83 @@ using Microsoft.VisualStudio.Shell;
 
 namespace CommentLinks.Commands
 {
-    public class CommentLinkCommandBase
-    {
-        public static readonly Guid CommandSet = new Guid("8b7f9d00-fcc1-4b0f-a951-3d63273c87de");
+	public class CommentLinkCommandBase
+	{
+		public static readonly Guid CommandSet = new Guid("8b7f9d00-fcc1-4b0f-a951-3d63273c87de");
 
 #pragma warning disable SA1401 // Fields should be private
-        protected CommentLinksPackage package;
+		protected CommentLinksPackage package;
 #pragma warning restore SA1401 // Fields should be private
 
-        protected IAsyncServiceProvider ServiceProvider
-        {
-            get
-            {
-                return this.package;
-            }
-        }
+		protected IAsyncServiceProvider ServiceProvider
+		{
+			get
+			{
+				return this.package;
+			}
+		}
 
-        protected string FormattedLinkText
-        {
-            get
-            {
-                switch (this.package.Options.LinkCasing)
-                {
-                    case CaseOption.TitleCase: return CultureInfo.CurrentUICulture.TextInfo.ToTitleCase(this.package.Options.TriggerWord);
-                    case CaseOption.UPPERCASE: return this.package.Options.TriggerWord.ToUpper();
-                    case CaseOption.lowercase:
-                    default:
-                        return this.package.Options.TriggerWord.ToLower();
-                }
-            }
-        }
+		protected string FormattedLinkText
+		{
+			get
+			{
+				switch (this.package.Options.LinkCasing)
+				{
+					case CaseOption.TitleCase: return CultureInfo.CurrentUICulture.TextInfo.ToTitleCase(this.package.Options.TriggerWord);
+					case CaseOption.UPPERCASE: return this.package.Options.TriggerWord.ToUpper();
+					case CaseOption.lowercase:
+					default:
+						return this.package.Options.TriggerWord.ToLower();
+				}
+			}
+		}
 
-        protected static string SimpleSpaceEncoding(string original)
-        {
-            return original.Replace(" ", "%20");
-        }
+		protected static string SimpleSpaceEncoding(string original)
+		{
+			return original.Replace(" ", "%20");
+		}
 
-        protected string GetFormattedFilePath(string filePath)
-        {
-            ThreadHelper.ThrowIfNotOnUIThread();
+		protected string GetFormattedFilePath(string filePath)
+		{
+			ThreadHelper.ThrowIfNotOnUIThread();
 
-            if (string.IsNullOrWhiteSpace(filePath) | !File.Exists(filePath))
-            {
-                return null;
-            }
+			if (string.IsNullOrWhiteSpace(filePath) | !File.Exists(filePath))
+			{
+				return null;
+			}
 
-            bool usingLongFileName = false;
+			if (this.package.Options.UseAbsolutePaths)
+			{
+				// Don't change anything. We want the entire file path.
+			}
+			else
+			{
+				bool usingLongFileName = false;
 
-            if (this.package.Options.IncludePathToFile)
-            {
-                var proj = ProjectHelpers.Dte.Solution?.FindProjectItem(filePath)?.ContainingProject;
+				if (this.package.Options.IncludePathToFile)
+				{
+					var proj = ProjectHelpers.Dte.Solution?.FindProjectItem(filePath)?.ContainingProject;
 
-                // Allow for items that aren't part of a project (such as solution items)
-                if (proj != null && !string.IsNullOrWhiteSpace(proj.FileName))
-                {
-                    var projDir = Path.GetDirectoryName(proj.FileName);
+					// Allow for items that aren't part of a project (such as solution items)
+					if (proj != null && !string.IsNullOrWhiteSpace(proj.FileName))
+					{
+						var projDir = Path.GetDirectoryName(proj.FileName);
 
-                    if (filePath.StartsWith(projDir))
-                    {
-                        filePath = filePath.Substring(projDir.Length).TrimStart('\\', '/');
-                        usingLongFileName = true;
-                    }
-                }
-            }
+						if (filePath.StartsWith(projDir))
+						{
+							filePath = filePath.Substring(projDir.Length).TrimStart('\\', '/');
+							usingLongFileName = true;
+						}
+					}
+				}
 
-            if (!usingLongFileName && !string.IsNullOrWhiteSpace(filePath))
-            {
-                filePath = Path.GetFileName(filePath);
-            }
+				if (!usingLongFileName && !string.IsNullOrWhiteSpace(filePath))
+				{
+					filePath = Path.GetFileName(filePath);
+				}
+			}
 
-            return SimpleSpaceEncoding(filePath);
-        }
-    }
+			return SimpleSpaceEncoding(filePath);
+		}
+	}
 }
--- src/OptionsGrid.cs
+++ src/OptionsGrid.cs
@@ -10,16 +10,6 @@ namespace CommentLinks
     public class OptionsGrid : DialogPage
     {
         [Category("General")]
-        [DisplayName("Include relative path to file")]
-        [Description("If set to true, links generated with the context menu will include the relative path to the file within a project.")]
-        public bool IncludePathToFile { get; set; } = false;
-
-        [Category("General")]
-        [DisplayName("Link text case")]
-        [Description("How the link indicator will be cased in links generated from the context menu.")]
-        public CaseOption LinkCasing { get; set; } = CaseOption.lowercase;
-
-        [Category("General")]
         [DisplayName("Trigger word")]
         [Description("The word used to use to indicate a link. Alphabetic characters only. Re-open documents to see the change.")]
         public string TriggerWord { get; set; } = "link";
@@ -29,7 +19,22 @@ namespace CommentLinks
         [Description("Running commands can have consequences. By using this functionality you accept the risk of any unexpected or unintended consequences.")]
         public bool AcceptRiskOfRunningCommands { get; set; } = false;
 
-        internal void EnsureValidTriggerWord()
+		[Category("Generated Links")]
+		[DisplayName("Include relative path to file")]
+		[Description("If set to true, links generated with the context menu will include the relative path to the file within a project.")]
+		public bool IncludePathToFile { get; set; } = false;
+
+		[Category("Generated Links")]
+		[DisplayName("Link text case")]
+		[Description("How the link indicator will be cased in links generated from the context menu.")]
+		public CaseOption LinkCasing { get; set; } = CaseOption.lowercase;
+
+		[Category("Generated Links")]
+		[DisplayName("Use absolute paths")]
+		[Description("Include the full path to a file in links generated from the context menu. This takes priority over including the relative path.")]
+		public bool UseAbsolutePaths { get; set; } = false;
+
+		internal void EnsureValidTriggerWord()
         {
             var temp = new System.Text.StringBuilder();
 
