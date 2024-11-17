6489be30b816e527728e969affaf6a2fbe76fa47
Matt Lacey <matt@mrlacey.co.uk>
2024-11-17 16:51:53 +0000

    Create AnalyticsConfig.cs

---
+++ src/AnalyticsConfig.cs
@@ -0,0 +1,10 @@
+﻿// Copyright (c) Matt Lacey Ltd. All rights reserved.
+// Licensed under the MIT license.
+
+namespace CommentLinks
+{
+	public static class AnalyticsConfig
+	{
+		public static string TelemetryConnectionString { get; set; } = "";
+	}
+}
